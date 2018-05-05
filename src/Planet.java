09c3637f6d237515083658abed16931ef3ee6c94
Unknown <jonaskhansen@gmail.com>
2018-05-05 17:43:08 +0200

    Added player in control to constructor

--- src/Planet.java
+++ src/Planet.java
@@ -1,11 +1,18 @@
+import units.Unit;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Planet {
 
     private String name;
     private int resourceProduction;
+    Player playerInControl;
 
     public Planet(String name, int resourceProduction) {
         this.name = name;
         this.resourceProduction = resourceProduction;
+        this.playerInControl = null;
     }
 
     public String getName() {
@@ -14,5 +21,13 @@ public class Planet {
 
     public int getResourceProduction() {
         return resourceProduction;
+    }
+
+    public Player getPlayerInControl() {
+        return playerInControl;
+    }
+
+    public void setPlayerInControl(Player playerInControl) {
+        this.playerInControl = playerInControl;
     }
 }
