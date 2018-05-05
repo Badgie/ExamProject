032ac7f1d3e362cf91c1f46519ede11ab0648de2
Unknown <jonaskhansen@gmail.com>
2018-05-05 18:53:21 +0200

    Fiddling with text formatting

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,18 @@ public class Main {
         system.removeShip(carrier);
         system.removeShip(cruiser);
         System.out.println(system.getShips());*/
+
+        /* System.out.println(String.format("%-8s %-15s %-15s %-15s %-15s %s", "Player:", "Unit",
+                "Combat Value", "Resource Cost", "Movement Speed", "Capacity"));
+        System.out.println(String.format("%-24s %-15s %-15s %-15s %s", "         Dreadnought",
+                "5", "2", "5", "0")); */
+
+        /* System.out.println(String.format("%-15s %s (%s)", "Green" + " Player:", "Pickle Rick", "Pickle"));
+        System.out.println(String.format("%15s %s", "", "Tiny Earth"));
+        System.out.println(String.format("%15s %s", "", "Planet on a Cob" + "\n"));
+
+        System.out.println(String.format("%-15s %s (%s)", "Red" + " Player:", "President Morty", "Mortian"));
+        System.out.println(String.format("%15s %s", "", "Citadel of Ricks"));
+        System.out.println(String.format("%15s %s", "", "Screaming Sun Planet")); */
     }
 }
