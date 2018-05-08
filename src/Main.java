3990c8a64c0246837045336361ce825c2142ead7
Unknown <jonaskhansen@gmail.com>
2018-05-08 20:16:51 +0200

    Added comment headers

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
-// import game.units.*;
+/*
+ * Made by:
+ * Jonas Krogh Hansen, Software
+ * jh17@student.aau.dk
+ */
 
 import game.galaxy.Galaxy;
+import game.systems.HexaSystem;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-        
-        Galaxy galaxy = new Galaxy().constructGalaxy();
-        System.out.println("Galaxy was succesfully constructed.");
+        Galaxy galaxy = new Galaxy();
+        galaxy.constructGalaxy(galaxy);
+        System.out.println("Galaxy was successfully constructed.");
         System.out.println("Systems: " + galaxy.getSystems().size());
         System.out.println("Planets: " + galaxy.getPlanets().size());
         System.out.println("Players: " + galaxy.getPlayers().size());
         System.out.println("Ships: " + galaxy.getShips().size());
         System.out.println("Checking if combat is necessary...");
-        galaxy.checkIfCombatIsNecessaryInSystems();
+        galaxy.checkIfCombatIsNecessaryInGalaxy();
         System.out.println("Creating planetary control file...");
         try {
             galaxy.createPlanetaryControlFile();
